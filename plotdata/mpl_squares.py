4d5952876fd1ed34f494829d4495eeb36fd0df07
kromites <1625876673@qq.com>
2020-04-13 21:46:41 +0800

    matplotlib learn

---
+++ plotdata/mpl_squares.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+input_value = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+plt.plot(input_value, squares, linewidth=5)
+
+# 设置图形标题，并给坐标轴加上标签
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of value", fontsize=14)
+
+# 刻度尺标记
+plt.tick_params(axis='both', which='major', labelsize=14)
+plt.show()
---
+++ plotdata/scatter_squares.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolors='none', s=40)
+# s是代表点的尺寸, c是代表颜色， cmap是说明使用哪个颜色渐变色
+
+# 设置图表标题并给坐标轴加上标签
+plt.title("Square Number", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# 设置刻度标记的大小
+plt.tick_params(axis='both', which='major', labelsize=14)
+
+# 设置每个坐标轴的取值范围
+plt.axis([0, 1100, 0, 1100000])
+
+plt.savefig('squares_plot.png', bbox_inches='tight')
+plt.show()
